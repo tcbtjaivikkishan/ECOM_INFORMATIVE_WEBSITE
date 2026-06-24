aa4a3abf1fa824ab6e7c55c028b8e30b378dc4c7
PARAG RAI <tcbtjaivikkisan@gmail.com>
2026-06-24 15:17:16 +0530

    redeploy

--- src/components/header/hero.tsx
+++ src/components/header/hero.tsx
@@ -3,6 +3,7 @@ import AnimatedHero from '@/components/header/HeroClient'
 export default function Hero() {
   const heroImages = [
     '/Home/banner1.webp',
+    '/Home/banner2.webp',
     '/Home/banner3.webp',
   ]
 
